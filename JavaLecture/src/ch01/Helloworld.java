d999eb025c9c1a308e5466a51a168440bdd92c63
lhj3248 <zlxwl@naver.com>
2023-06-20 16:56:50 +0900

    first commit

---
+++ JavaLecture/src/ch01/Helloworld.java
@@ -0,0 +1,12 @@
+package ch01;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!!");
+		System.out.println("안녕하세요?");
+
+	}
+
+}
